687ce92391b4349ce1ae2ba7dce811e3a629b38c
Derek Ashmore <dashmore@force66.com>
2016-09-25 09:56:41 -0500

    Added licensing note

--- StackWise-core/src/main/scala/guru/monolith/stackwise/core/StackWiseApplication.scala
+++ StackWise-core/src/main/scala/guru/monolith/stackwise/core/StackWiseApplication.scala
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Break The Monolith, Derek C. Ashmore and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *    Derek C. Ashmore - initial API and implementation
+ *******************************************************************************/
 package guru.monolith.stackwise.core
 
 import org.apache.commons.cli.Options
